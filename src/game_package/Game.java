c26896bad1f7b6baedeeb29cf40d30012945d7fe
Rahul Anilkumar <rahulanilkumar8@gmail.com>
2018-10-24 09:40:47 -0400

    Added the first phase of the game

--- src/game_package/Game.java
+++ src/game_package/Game.java
@@ -1,5 +1,40 @@
 package game_package;
 
-public class Game {
+import level_package.Level;
 
+import java.util.List;
+
+import entity_package.Entity;
+import entity_package.Sunflower;
+
+public class Game {
+	
+	Level level;
+	int sunPoints;
+	int numSunSpawn;
+	int gamePhase;
+	List<Entity> entity;
+		
+	
+	/**
+	 * Gathers sunshine from all sunflowers and the randomly spawned sunshine to the total count
+	 */
+	public void sunshinePhase() {
+		System.out.println("In Progress... Gathering Sunshine!");
+		if(numSunSpawn !=0) {
+			sunPoints += 25;
+		}
+		for(Entity x: entity) {
+			if(x instanceof Sunflower) {
+				sunPoints += 25;
+			}
+		}
+	}
+	
+	public void userPhase() {
+		
+		
+	}
+	
+	
 }
